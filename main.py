b53fae1eaac5e8922f86095fb124b5d9df7da896
Matheus Almeida Santos Mendonça <matheus.mendonca@lmcorp.local>
2023-04-26 19:35:24 -0300

    feat: Protótipo versão 1.2 - Adicionando leitura de novos campos, melhorando os existentes e separando processos em funções

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import yaml
 
+def trocar_variavel_pelo_valor(texto: str):
+    try:
+        if texto in objetos_global.keys():
+            return objetos_global[texto]
+        elif texto in objetos_tarefa.keys():
+            return objetos_tarefa[texto]
+        else:
+            palavras = texto.split()
+        for palavra in palavras:
+            if palavra in objetos_global.keys():
+                texto = f'({texto.replace(palavra, str(objetos_global[palavra]))})'
+            if palavra in objetos_tarefa.keys():
+                texto = f'({texto.replace(palavra, str(objetos_tarefa[palavra]))})'
+        return texto
+    except Exception as erro:
+        raise Exception(f'Erro na função trocar_variavel_pelo_valor: {erro}')
+
+
+def criar_variaveis(lista_variaveis: list) -> dict:
+    objetos = {}
+    for variavel in lista_variaveis:
+        for valor in variavel.values():
+            objetos[valor['name']] = valor['initialValue']['noValue'] if valor['initialValue'].get('noValue') else valor['initialValue']['lit']
+    return objetos
+
+
 def quebrar_dicionario(objeto, lista: list):
+    finaliza_loop = False
+    proxima_task = None
     try:
         if type(objeto) == list:
             for obj in objeto:
-                quebrar_dicionario(obj, lista)
+                _ , finaliza_loop, proxima_task = quebrar_dicionario(obj, lista)
         elif type(objeto) == dict:
             for chave, valor in objeto.items():
                 match chave:
@@ -21,24 +49,26 @@ def quebrar_dicionario(objeto, lista: list):
                         if len(valor['attributes']) == 1:
                             atributo_name = valor['attributes'][0]['attribute']['name']['lit']
                             atributo_value = valor['attributes'][0]['attribute']['value']['exp']
-                            atributo_value = objetos_global[atributo_value] if atributo_value in objetos_global.keys() else atributo_value
+                            atributo_value = trocar_variavel_pelo_valor(atributo_value)
                             lista.extend([f'Atributo {atributo_name} = {atributo_value}'])
                         else:
                             for atributo in valor['attributes']:
                                 print(atributo)
                     case 'decision':
                         condition = valor['condition']['exp']
+                        condition = trocar_variavel_pelo_valor(condition)
                         name = valor['name']
                         saida = valor.get('outputs')
                         if saida is None:
-                            raise Exception(f'erro decision {name}: {chave} = {valor}, {acao}')
-                        chave = input(f'yes/no\ncondição: {condition} é verdadeira? ').lower()
-                        quebrar_dicionario(saida[chave]['actions'], lista)
+                            raise Exception(f'erro decision {name}: {chave} = {valor}, {objeto}')
+                        chave = input(f'condição: {condition} é verdadeira? yes/no ').lower()
+                        _ , finaliza_loop, proxima_task = quebrar_dicionario(saida[chave]['actions'], lista)
                     case 'disconnect':
+                        finaliza_loop = True
                         lista.extend([valor['name']])
                     case 'jumpToTask':
                         name_task = valor['name']
-                        ref_task = valor['targetTaskRef']
+                        proxima_task = ref_task = valor['targetTaskRef']
                         lista.extend([f'{name_task} = {ref_task}'])
                     case 'updateData':
                         name_data = valor['name']
@@ -48,6 +78,8 @@ def quebrar_dicionario(objeto, lista: list):
                                 valor_var = valor['value']['lit'] if valor['value'].get('lit') != None else valor['value']['exp']
                                 auxiliar.append([chave, valor['variable'], valor_var])
                         for tipo_var, nome_var, valor_var in auxiliar:
+                            if type(valor_var) == str:
+                                valor_var = trocar_variavel_pelo_valor(valor_var)
                             objetos_global[nome_var] = valor_var
                             lista.extend([f'{name_data} - {nome_var}: {tipo_var} = {valor_var}'])
                     case 'setWrapupCode':
@@ -57,23 +89,22 @@ def quebrar_dicionario(objeto, lista: list):
                         for case in valor['evaluate']['firstTrue']['cases']:
                             exp = case['case']['value']['exp']
                             lista.extend([f'case {exp}'])
-                            quebrar_dicionario(case['case']['actions'], lista)
+                            _ , finaliza_loop, proxima_task = quebrar_dicionario(case['case']['actions'], lista)
                     case 'collectInput':
                         prompt = valor['inputAudio']['prompt'] if valor['inputAudio'].get('prompt') else valor['inputAudio']['tts']
-                        print(f'Tocou o prompt: {prompt}')
+                        print(f'\nTocou o prompt: {prompt}')
                         name_var = valor['inputData']['var']
                         value_var = input(f'Digite a entrada: ')
                         objetos_global[name_var] = value_var
-                        lista.extend([f'{name_var} = "{value_var}"'])
                         if value_var != "":
-                            quebrar_dicionario(valor['outputs']['success']['actions'], lista)
+                            _ , finaliza_loop, proxima_task = quebrar_dicionario(valor['outputs']['success']['actions'], lista)
                         else:
-                            quebrar_dicionario(valor['outputs']['failure']['actions'], lista)
+                            _ , finaliza_loop, proxima_task = quebrar_dicionario(valor['outputs']['failure']['actions'], lista)
                     case _:
                         print(chave, valor)
                         if type(valor) == list or type(valor) == dict:
-                            quebrar_dicionario(valor, lista) 
-        return lista
+                            _ , finaliza_loop, proxima_task = quebrar_dicionario(valor, lista) 
+        return lista, finaliza_loop, proxima_task
     except Exception as erro:
         print('ERRO')
         print(erro)
@@ -85,16 +116,22 @@ with open('Testes_Matheus_v2-0.yaml', "r", encoding="utf-8") as arquivo:
     data = yaml.safe_load(arquivo)
 
 
-objetos_global = {}
+objetos_global = criar_variaveis(data['inboundCall']['variables'])
+tasks = {f'/inboundCall/tasks/task[{task["task"]["refId"]}]': task['task'] for task in data['inboundCall']['tasks']}
+task_loop = data['inboundCall']['tasks'][0]['task']
 
-for task in data['inboundCall']['tasks']:
-    for acao in task['task']['actions']:
-        lista = quebrar_dicionario(acao, [])
-        match len(lista):
-            case 0:
-                pass
-                #print(f'lista vazia pela ação: {acao}')
-            case _:
-                for task in lista:
-                    print(task)
-        
+
+while True:
+    objetos_tarefa = criar_variaveis(task_loop['variables'])
+    for objeto in task_loop['actions']:
+        lista, finaliza_loop, proxima_task = quebrar_dicionario(objeto, [])
+        for item in lista:
+            print(item)
+        if proxima_task != None:
+            if tasks[proxima_task] != task_loop:
+                objetos_tarefa.clear()
+            task_loop = tasks[proxima_task]
+            break
+        if finaliza_loop:
+            exit()
+                
